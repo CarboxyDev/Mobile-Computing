f78b3fc2ac96bdc975eeaaf613b6c0c793594c5e
Arman <37100541+CarboxyDev@users.noreply.github.com>
2024-03-28 19:02:24 +0530

    Setup A2 project with boilerplate

---
+++ A1/README.md
@@ -0,0 +1,9 @@
+## Assignment 1 
+
+- This application uses Jetpack compose as the development toolkit for the UI.
+- The app uses custom styles for the UI
+- Composables have been used in the assignment wherever it made sense (if the component was reusable or atomic in nature)
+- Components include LocationColumn (lazy/normal), LocationEntry, AtomicButton and AtomicProgressBar
+- The list of locations has been hardcoded and the toggle between lazy UI and normal non-lazy UI is hardcoded within the code
+- The app has been tested on Pixel 6, Pixel 3a and my own android phone Redmi 10s
+
---
+++ A2/app/src/main/java/com/arman/assignment2/ui/theme/Color.kt
@@ -0,0 +1,39 @@
+package com.arman.assignment2.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+object Colors {
+    val white = Color(0xFFFFFFFF)
+
+    val sky50 = Color(0xFFf0f9ff)
+    val sky100 = Color(0xFFe0f2fe)
+    val sky200 = Color(0xFFbae6fd)
+    val sky300 = Color(0xFF7dd3fc)
+    val sky400 = Color(0xFF38bdf8)
+    val sky500 = Color(0xFF0ea5e9)
+    val sky600 = Color(0xFF0284c7)
+    val sky700 = Color(0xFF0369a1)
+    val sky800 = Color(0xFF075985)
+    val sky900 = Color(0xFF0c4a6e)
+    val sky950 = Color(0xFF082f49)
+
+    val zinc50 = Color(0xFFfafafa)
+    val zinc100 = Color(0xFFf4f4f5)
+    val zinc200 = Color(0xFFe4e4e7)
+    val zinc300 = Color(0xFFd4d4d8)
+    val zinc400 = Color(0xFFa1a1aa)
+    val zinc500 = Color(0xFF71717a)
+    val zinc600 = Color(0xFF52525b)
+    val zinc700 = Color(0xFF3f3f46)
+    val zinc800 = Color(0xFF1F1F23)
+    val zinc900 = Color(0xFF18181b)
+    val zinc950 = Color(0xFF09090b)
+}
