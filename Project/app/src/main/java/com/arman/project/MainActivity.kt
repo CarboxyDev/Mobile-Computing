8a0a54a8f0aeb0a45ce95340dd5c9a80b9de73f0
Arman <37100541+CarboxyDev@users.noreply.github.com>
2024-05-13 13:32:57 +0530

    Add project boilerplate

---
+++ Project/app/src/main/java/com/arman/project/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.arman.project
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.arman.project.ui.theme.Colors
+import com.arman.project.ui.theme.ProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProjectTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainApplication()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainApplication() {
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Colors.zinc900)
+        .padding(top = 64.dp, bottom = 18.dp, start = 16.dp, end = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ) {}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ProjectTheme {
+        MainApplication()
+    }
+}
---
+++ Project/app/src/main/java/com/arman/project/ui/components/AtomicButton.kt
@@ -0,0 +1,44 @@
+package com.arman.project.ui.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.arman.project.ui.theme.Colors
+
+
+
+@Composable
+fun AtomicButton(content: String, size: Pair<Dp, Dp>, fullWidth: Boolean? = false, outline: Boolean? = false, onClick: () -> Unit) {
+    val modifier = if (fullWidth == true) {
+        Modifier.fillMaxWidth().height(size.second);
+    } else {
+        Modifier.size(size.first, size.second)
+    }
+
+    if (outline == true) {
+        Button(onClick = onClick, colors = ButtonDefaults.outlinedButtonColors(
+            contentColor = Colors.sky500
+        ), modifier = modifier.shadow(4.dp).border(width = 2.dp, color = Colors.sky500, shape = RoundedCornerShape(4.dp))) {
+            Text(text = content, fontSize = 18.sp)
+        }
+        return
+    }
+
+    Button(onClick = onClick, colors = ButtonDefaults.buttonColors(
+        containerColor = Colors.sky500,
+        contentColor = Colors.white
+    ), modifier = modifier.shadow(4.dp), shape = RoundedCornerShape(4.dp)) {
+        Text(text = content, fontSize = 18.sp)
+    }
+}
