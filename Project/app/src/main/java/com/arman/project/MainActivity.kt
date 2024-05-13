3bf01e229aa72aef2b063d7101993e8a94da7870
Arman <37100541+CarboxyDev@users.noreply.github.com>
2024-05-13 14:27:46 +0530

    Make a functioning app

--- Project/app/src/main/java/com/arman/project/MainActivity.kt
+++ Project/app/src/main/java/com/arman/project/MainActivity.kt
@@ -3,25 +3,47 @@ package com.arman.project
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.arman.project.ui.theme.Colors
 import com.arman.project.ui.theme.ProjectTheme
+import org.jitsi.meet.sdk.BroadcastEvent
+import org.jitsi.meet.sdk.BroadcastIntentHelper
 import org.jitsi.meet.sdk.JitsiMeet
+import org.jitsi.meet.sdk.JitsiMeetActivity
 import org.jitsi.meet.sdk.JitsiMeetConferenceOptions
+import timber.log.Timber
 import java.net.MalformedURLException
 import java.net.URL
 
@@ -67,10 +89,74 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+    override fun onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver)
+        super.onDestroy()
+    }
+
+    fun onButtonClick(v: View?) {
+        val editText = findViewById<EditText>(
+            R.id.conferenceName
+        );
+        val text = editText.text.toString()
+        if (text.isNotEmpty()) {
+            // Build options object for joining the conference. The SDK will merge the default
+            // one we set earlier and this one when joining.
+            val options = JitsiMeetConferenceOptions.Builder()
+                .setRoom(text)
+                // Settings for audio and video
+                //.setAudioMuted(true)
+                //.setVideoMuted(true)
+                .build()
+            // Launch the new activity with the given options. The launch() method takes care
+            // of creating the required Intent and passing the options.
+            JitsiMeetActivity.launch(this, options)
+        }
+    }
+
+
+    private fun registerForBroadcastMessages() {
+        val intentFilter = IntentFilter()
+
+        /* This registers for every possible event sent from JitsiMeetSDK
+           If only some of the events are needed, the for loop can be replaced
+           with individual statements:
+           ex:  intentFilter.addAction(BroadcastEvent.Type.AUDIO_MUTED_CHANGED.action);
+                intentFilter.addAction(BroadcastEvent.Type.CONFERENCE_TERMINATED.action);
+                ... other events
+         */
+        for (type in BroadcastEvent.Type.values()) {
+            intentFilter.addAction(type.action)
+        }
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, intentFilter)
+    }
+
+    // Example for handling different JitsiMeetSDK events
+    private fun onBroadcastReceived(intent: Intent?) {
+        if (intent != null) {
+            val event = BroadcastEvent(intent)
+            when (event.type) {
+                BroadcastEvent.Type.CONFERENCE_JOINED -> Timber.i("Conference Joined with url%s", event.getData().get("url"))
+                BroadcastEvent.Type.PARTICIPANT_JOINED -> Timber.i("Participant joined%s", event.getData().get("name"))
+                else -> Timber.i("Received event: %s", event.type)
+            }
+        }
+    }
+
+    // Example for sending actions to JitsiMeetSDK
+    private fun hangUp() {
+        val hangupBroadcastIntent: Intent = BroadcastIntentHelper.buildHangUpIntent()
+        LocalBroadcastManager.getInstance(this.applicationContext).sendBroadcast(hangupBroadcastIntent)
+    }
+
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainApplication() {
+    var conferenceName by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
     Column(modifier = Modifier
         .fillMaxSize()
@@ -79,7 +165,40 @@ fun MainApplication() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
 
-    ) {}
+    ) {
+        TextField(
+            value = conferenceName,
+            onValueChange = { conferenceName = it },
+            label = { Text("Enter conference name") },
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = Colors.white,
+                unfocusedContainerColor = Colors.zinc800,
+                focusedIndicatorColor = Colors.sky500,
+                unfocusedIndicatorColor = Colors.sky500
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Button(
+            onClick = {
+                if (conferenceName.isNotEmpty()) {
+                    val options = JitsiMeetConferenceOptions.Builder()
+                        .setRoom(conferenceName)
+                        .build()
+                    JitsiMeetActivity.launch(context, options)
+                } else {
+                    Toast.makeText(context, "Please enter a conference name", Toast.LENGTH_SHORT).show()
+                }
+            },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("Join Conference")
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
